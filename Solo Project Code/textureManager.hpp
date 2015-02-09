a2d2408112cdf52282f0c6010c624c71c8cb32f4
JBRPG <mrjbrpg@gmail.com>
2015-02-09 14:50:10 -0800

    Texture manager
    Texture manager added

---
+++ Solo Project Code/textureManager.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <map>
+
+class TextureManager{
+
+private:
+	// Texture array used
+	std::map<std::string, sf::Texture> textures;
+
+public:
+
+	// Add texture from file
+	void loadTexture(const std::string& name, const std::string& filename);
+
+	// Translate the id into reference
+	sf::Texture& getRef(const std::string& texture);
+
+	// Constructor
+	TextureManager();
+
+};
