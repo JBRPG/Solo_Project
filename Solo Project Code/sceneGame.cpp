e7064e16dd78275cfc0f5091b648bd508d97fc22
JBRPG <mrjbrpg@gmail.com>
2015-02-09 15:40:30 -0800

    Scene Game
    Added barebones version of Scene Game, inherited from Scene

---
+++ Solo Project Code/sceneGame.cpp
@@ -0,0 +1,33 @@
+#include "sceneGame.hpp"
+
+SceneGame::SceneGame(Game& game){
+
+	this->game = game;
+
+	// Set up the view layers
+
+	sf::Vector2f pos = sf::Vector2f(this->game.window.getSize());
+	this->background1View.setSize(pos);
+	this->background2View.setSize(pos);
+	this->gameView.setSize(pos);
+	this->playerView.setSize(pos);
+	this->hudView.setSize(pos);
+	pos *= 0.5f;
+	this->background1View.setCenter(pos);
+	this->background2View.setCenter(pos);
+	this->gameView.setCenter(pos);
+	this->playerView.setCenter(pos);
+	this->hudView.setCenter(pos);
+
+	// We will fill in other things later
+}
+
+void SceneGame::draw(sf::Time dt){
+	// draw the entities
+
+}
+
+void SceneGame::update(sf::Time){
+	///update the entities
+
+}
---
+++ Solo Project Code/sceneGame.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML\Graphics.hpp>
+
+#include "scene.hpp"
+#include "game.hpp"
+
+class SceneGame : public Scene{
+private:
+
+	sf::View  background1View;
+	sf::View  background2View;
+	sf::View  gameView;
+	sf::View  playerView;
+	sf::View  hudView;
+
+public:
+	SceneGame(Game& game);
+
+	virtual void draw(sf::Time dt);
+	virtual void update(sf::Time dt);
+
+};
